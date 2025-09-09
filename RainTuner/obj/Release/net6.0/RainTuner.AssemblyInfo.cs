ab04011938c4f14eaf50d5ee252474e1df7b7e6a
Anthony <anthonyellenbroek@gmail.com>
2025-09-09 08:52:37 +0200

    Update README.md with detailed features and installation instructions; add new .gitignore entries; update assembly informational version

--- RainTuner/obj/Release/net6.0/RainTuner.AssemblyInfo.cs
+++ RainTuner/obj/Release/net6.0/RainTuner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RainTuner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e9f9a449e70cc6559ef3ebc59961618e3b80485")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1be08b120f9d0799651f3a60d1becbd9cac84300")]
 [assembly: System.Reflection.AssemblyProductAttribute("RainTuner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RainTuner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
